bf9d80d0d2167477a103be0e05c351020aef083b
pranto <kumar.pranto@northsouth.edu>
2021-03-28 23:44:51 +0600

    2nd commit

--- letconst.js
+++ letconst.js
@@ -5,8 +5,16 @@ const numbers = [12,45];
 numbers[1] = 88;
 numbers.push(12);
 const nayok = {name : "Sakib Khan",phone : 12380};
-//console.log(numbers);
+console.log(numbers);
 
 let patientName = "Rahim Cacha";
 patientName = "Fatema Khala";
 console.log(patientName);
+
+let sum = 0;
+for(let i = 0; i < 0; i++){
+    sum = sum + i;
+}
+console.log(i);
+
+
