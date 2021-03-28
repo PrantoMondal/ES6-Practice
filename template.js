93126cdfabc8e94455f6fd40b5da376dfea1a849
pranto <kumar.pranto@northsouth.edu>
2021-03-29 00:11:40 +0600

    template string & Multiline string

---
+++ template.js
@@ -0,0 +1,16 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName +" "+ lastName+" is a good boy";
+const fullName2 = `${firstName} ${20+50+30} is a good boy.`;
+console.log(fullName2);
+
+
+const multiLine = "I Love You\n"
+    +"I Miss You\n"
+    +"I Need You";
+console.log(multiLine);
+
+const multiLine2 = `I love you
+I miss you
+no. I don't need you`
+console.log(multiLine2);
