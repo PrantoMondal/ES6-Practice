eeea856545d689bbc265eab662a1b86816e33749
pranto <kumar.pranto@northsouth.edu>
2021-03-28 22:38:04 +0600

    Hizibizi

---
+++ letconst.js
@@ -0,0 +1,12 @@
+const hubby = "Elias Kanchon";
+console.log(hubby);
+
+const numbers = [12,45];
+numbers[1] = 88;
+numbers.push(12);
+const nayok = {name : "Sakib Khan",phone : 12380};
+//console.log(numbers);
+
+let patientName = "Rahim Cacha";
+patientName = "Fatema Khala";
+console.log(patientName);
